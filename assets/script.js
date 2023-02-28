8970aa00b4b15b53cc2f710dce45e5446763fdfa
clarence_c <clarence@biteline.net>
2023-02-27 16:54:29 -0800

    access openweather api

--- assets/script.js
+++ assets/script.js
@@ -1,124 +1,56 @@
-// save reference to important DOM elements
-var timeDisplayEl = $('#time-display');
-var projectDisplayEl = $('#project-display');
-var projectFormEl = $('#project-form');
-var projectNameInputEl = $('#project-name-input');
-var projectTypeInputEl = $('#project-type-input');
-var projectDateInputEl = $('#project-date-input');
-
-// handle displaying the time
-function displayTime() {
-  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
-  timeDisplayEl.text(rightNow);
+async function getWeather() {
+    // get intake form
+    // input into URL
+    
+    let requestedURL = 'https://api.openweathermap.org/data/2.5/weather?lat=39.8865&lon=-83.4483&appid=6d5b483be1e33702dfcdcba9ef8ea047'
+    let results = await fetch(requestedURL); 
+    let data = await results.json();
+    
+    let temp = 1.8*(data.main.temp - 273) + 32; 
+    let windSpeed = data.wind.speed; 
+    let humidity = data.main.humidity; 
 }
 
-// Reads projects from local storage and returns array of project objects.
-// Returns an empty array ([]) if there aren't any projects.
-function readProjectsFromStorage() {
-  var projects = localStorage.getItem('projects');
-  if (projects) {
-    projects = JSON.parse(projects);
-  } else {
-    projects = [];
-  }
-  return projects;
+// getWeather();
+
+// $("#InputCity").val() = "Hello";
+
+function getInput() {
+
+    // console.log(userInput);
+    $("button").click(async function () {
+        let userInput  = document.querySelector('input').value;
+        let geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q='.concat(userInput, '&appid=6d5b483be1e33702dfcdcba9ef8ea047');
+        let results = await fetch(geoURL); 
+        let data = await results.json(); 
+        
+        let lat = data[0].lat;
+        let lon = data[0].lon;
+
+        let mainURL = 'https://api.openweathermap.org/data/2.5/weather?lat='.concat(lat,'&','lon=',lon, '&appid=6d5b483be1e33702dfcdcba9ef8ea047')
+        let mainResults = await fetch(mainURL); 
+        let mainData = await mainResults.json();
+        
+        let temp = 1.8*(mainData.main.temp - 273) + 32; 
+        let windSpeed = mainData.wind.speed; 
+        let humidity = mainData.main.humidity; 
+
+
+
+    })
+
 }
 
-// Takes an array of projects and saves them in localStorage.
-function saveProjectsToStorage(projects) {
-  localStorage.setItem('projects', JSON.stringify(projects));
-}
+getInput();
 
-// Gets project data from local storage and displays it
-function printProjectData() {
-  // clear current projects on the page
-  projectDisplayEl.empty();
 
-  // get projects from localStorage
-  var projects = readProjectsFromStorage();
+// Set up basic HTML and CSS stucture - leverage bootstrap when needed
+// Make a call to the weather map API
+    // - use the geocoding API to determine lat and lon of a city
+    // make sure 
 
-  // loop through each project and create a row
-  for (var i = 0; i < projects.length; i += 1) {
-    var project = projects[i];
-    var projectDate = dayjs(project.date);
-    // get date/time for start of today
-    var today = dayjs().startOf('day');
+// Have the user put in the city
+// the api returns the city call with specific parameters
+// use the appropriate html id tags to identify each 
 
-    // Create row and columns for project
-    var rowEl = $('<tr>');
-    var nameEL = $('<td>').text(project.name);
-    var typeEl = $('<td>').text(project.type);
-    var dateEl = $('<td>').text(projectDate.format('MM/DD/YYYY'));
-
-    // Save the index of the project as a data-* attribute on the button. This
-    // will be used when removing the project from the array.
-    var deleteEl = $(
-      '<td><button class="btn btn-sm btn-delete-project" data-index="' +
-        i +
-        '">X</button></td>'
-    );
-
-    // add class to row by comparing project date to today's date
-    if (projectDate.isBefore(today)) {
-      rowEl.addClass('project-late');
-    } else if (projectDate.isSame(today)) {
-      rowEl.addClass('project-today');
-    }
-
-    // append elements to DOM to display them
-    rowEl.append(nameEL, typeEl, dateEl, deleteEl);
-    projectDisplayEl.append(rowEl);
-  }
-}
-
-// Removes a project from local storage and prints the project data
-function handleDeleteProject() {
-  var projectIndex = parseInt($(this).attr('data-index'));
-  var projects = readProjectsFromStorage();
-  // remove project from the array
-  projects.splice(projectIndex, 1);
-  saveProjectsToStorage(projects);
-
-  // print projects
-  printProjectData();
-}
-
-// Adds a project to local storage and prints the project data
-function handleProjectFormSubmit(event) {
-  event.preventDefault();
-
-  // read user input from the form
-  var projectName = projectNameInputEl.val().trim();
-  var projectType = projectTypeInputEl.val(); // don't need to trim select input
-  var projectDate = projectDateInputEl.val(); // yyyy-mm-dd format
-
-  var newProject = {
-    name: projectName,
-    type: projectType,
-    date: projectDate,
-  };
-
-  // add project to local storage
-  var projects = readProjectsFromStorage();
-  projects.push(newProject);
-  saveProjectsToStorage(projects);
-
-  // print project data
-  printProjectData();
-
-  // clear the form inputs
-  projectNameInputEl.val('');
-  projectTypeInputEl.val('');
-  projectDateInputEl.val('');
-}
-
-projectFormEl.on('submit', handleProjectFormSubmit);
-
-// Use jQuery event delegation to listen for clicks on dynamically added delete
-// buttons.
-projectDisplayEl.on('click', '.btn-delete-project', handleDeleteProject);
-
-displayTime();
-setInterval(displayTime, 1000);
-
-printProjectData();
+// store current state into local storage
