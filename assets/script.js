4cfbe17d59854707115c5ccf7bd2f49996aeef2d
clarence_c <clarence@biteline.net>
2023-03-02 15:26:39 -0800

    debugging appending forecast

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,44 @@
-function getInput() {
-    $("button").click(async function () {
-        let userInput  = document.querySelector('input').value;
-        let geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q='.concat(userInput, '&appid=6d5b483be1e33702dfcdcba9ef8ea047');
-        let results = await fetch(geoURL); 
-        let data = await results.json(); 
-        
-        let lat = data[0].lat;
-        let lon = data[0].lon;
 
+let inputList = []
+
+async function getInput() {
+    let userInput  = document.querySelector('input').value;
+    let geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q='.concat(userInput, '&appid=6d5b483be1e33702dfcdcba9ef8ea047');
+    let results = await fetch(geoURL); 
+    let data = await results.json(); 
+    
+    let lat = data[0].lat;
+    let lon = data[0].lon;
+
+
+
+    //makes sure the city has not already been called
+    if (inputList.includes(userInput)) {
+        alert("Put in a new city!")
+    } else {
+        inputList.push(userInput); 
         currentWeather(lat,lon,userInput);
+        $(`.list-group`).append(`<button type="button" class="btn btn-secondary">` + userInput + `</button>`);
+    }
 
-    })
+    $('.btn-secondary').click(buttonInput);
 }
+
+
+async function buttonInput(event) {
+    // let userInput  = document.querySelector('input').value;
+    let userInput = event.target.textContent;
+    let geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q='.concat(userInput, '&appid=6d5b483be1e33702dfcdcba9ef8ea047');
+    let results = await fetch(geoURL); 
+    let data = await results.json(); 
+    
+    let lat = data[0].lat;
+    let lon = data[0].lon;
+
+    currentWeather(lat,lon,userInput);
+
+}
+
 
 async function currentWeather(lat, lon, userInput) {
     let mainURL = 'https://api.openweathermap.org/data/2.5/weather?lat='.concat(lat,'&','lon=',lon, '&appid=6d5b483be1e33702dfcdcba9ef8ea047')
@@ -31,6 +58,7 @@ async function currentWeather(lat, lon, userInput) {
 
         var now = dayjs(); 
         $('#title-city-box').text(userInput + now.format(' (M/D/YYYY)'));
+
         forecastWeather(lat, lon);
 
 }
@@ -42,48 +70,155 @@ async function forecastWeather(lat, lon) {
 
     let noonArray = []
 
+    //finds all noon times
     for (var i=0; i<forecastData.list.length; i++) {
         if (forecastData.list[i].dt_txt.includes("12:00:00")) {
             noonArray.push(forecastData.list[i]);
         }
     };
 
-    console.log(noonArray);
 
     for (var i=0; i<noonArray.length; i++) {
-        $('#forecast').append(`<div class="col" id=card-` + [i] + `></div>`);    
-
-        let timeStamp = dayjs(noonArray[i].dt*1000).format('MM/DD/YYYY');
         
+        let timeStamp = dayjs(noonArray[i].dt*1000).format('MM/DD/YYYY');
         let icon = noonArray[i].weather[0].icon; 
         let iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
-        
         let temp = 1.8*(noonArray[i].main.temp - 273) + 32; 
-
         let windSpeed = noonArray[i].wind.speed; 
-        
         let humidity = noonArray[i].main.humidity; 
-
-        $(`#card-`+[i]).append(`<p class=timeStamp>`+ timeStamp + `</p>`);
-        $(`#card-`+[i]).append(`<img id="wicon" src="` + iconurl + `" alt="Weather icon"></img>`);
-        $(`#card-`+[i]).append(`<p>Temperature: ` + temp.toFixed(2) + `\u00B0F` + `</p>`);
-        $(`#card-`+[i]).append(`<p>Wind: ` + windSpeed.toFixed(2) + ` MPH` + `</p>`);
-        $(`#card-`+[i]).append(`<p>Humidity: ` + humidity.toFixed(2) + ` %` + `</p>`);
-
-    }
+        
     
+        $('#forecast').append(`<div class="col" id=card-` + [i] + `></div>`);    
+        
+        // if new element is equal
+
+        if (inputList.length >= 1) {
+            $('#forecast').innerHTML = ' ';
+            $(`#card-`+[i]).append(`<p class=timeStamp>`+ timeStamp + `</p>`);
+            $(`#card-`+[i]).append(`<img id="wicon" src="` + iconurl + `" alt="Weather icon"></img>`);
+            $(`#card-`+[i]).append(`<p>Temperature: ` + temp.toFixed(2) + `\u00B0F` + `</p>`);
+            $(`#card-`+[i]).append(`<p>Wind: ` + windSpeed.toFixed(2) + ` MPH` + `</p>`);
+            $(`#card-`+[i]).append(`<p>Humidity: ` + humidity.toFixed(2) + ` %` + `</p>`);
+        } else {
+            $(`#card-`+[i]).append(`<p class=timeStamp>`+ timeStamp + `</p>`);
+            $(`#card-`+[i]).append(`<img id="wicon" src="` + iconurl + `" alt="Weather icon"></img>`);
+            $(`#card-`+[i]).append(`<p>Temperature: ` + temp.toFixed(2) + `\u00B0F` + `</p>`);
+            $(`#card-`+[i]).append(`<p>Wind: ` + windSpeed.toFixed(2) + ` MPH` + `</p>`);
+            $(`#card-`+[i]).append(`<p>Humidity: ` + humidity.toFixed(2) + ` %` + `</p>`);
+        }
+        
+    
+    } 
+
 }
 
-getInput();
+
+$('.btn-primary').click(getInput);
 
 
-// Set up basic HTML and CSS stucture - leverage bootstrap when needed
-// Make a call to the weather map API
-    // - use the geocoding API to determine lat and lon of a city
-    // make sure 
 
-// Have the user put in the city
-// the api returns the city call with specific parameters
-// use the appropriate html id tags to identify each 
 
-// store current state into local storage
+
+// var userFormEl = document.querySelector('#user-form');
+// var languageButtonsEl = document.querySelector('#language-buttons');
+// var nameInputEl = document.querySelector('#username');
+// var repoContainerEl = document.querySelector('#repos-container');
+// var repoSearchTerm = document.querySelector('#repo-search-term');
+
+// var formSubmitHandler = function (event) {
+//   event.preventDefault();
+
+//   var username = nameInputEl.value.trim();
+
+//   if (username) {
+//     getUserRepos(username);
+
+//     repoContainerEl.textContent = '';
+//     nameInputEl.value = '';
+//   } else {
+//     alert('Please enter a GitHub username');
+//   }
+// };
+
+// var buttonClickHandler = function (event) {
+//   var language = event.target.getAttribute('data-language');
+
+//   if (language) {
+//     getFeaturedRepos(language);
+
+//     repoContainerEl.textContent = '';
+//   }
+// };
+
+// var getUserRepos = function (user) {
+//   var apiUrl = 'https://api.github.com/users/' + user + '/repos';
+
+//   fetch(apiUrl)
+//     .then(function (response) {
+//       if (response.ok) {
+//         console.log(response);
+//         response.json().then(function (data) {
+//           console.log(data);
+//           displayRepos(data, user);
+//         });
+//       } else {
+//         alert('Error: ' + response.statusText);
+//       }
+//     })
+//     .catch(function (error) {
+//       alert('Unable to connect to GitHub');
+//     });
+// };
+
+// var getFeaturedRepos = function (language) {
+//   var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
+
+//   fetch(apiUrl).then(function (response) {
+//     if (response.ok) {
+//       response.json().then(function (data) {
+//         displayRepos(data.items, language);
+//       });
+//     } else {
+//       alert('Error: ' + response.statusText);
+//     }
+//   });
+// };
+
+// var displayRepos = function (repos, searchTerm) {
+//   if (repos.length === 0) {
+//     repoContainerEl.textContent = 'No repositories found.';
+//     return;
+//   }
+
+//   repoSearchTerm.textContent = searchTerm;
+
+//   for (var i = 0; i < repos.length; i++) {
+//     var repoName = repos[i].owner.login + '/' + repos[i].name;
+
+//     var repoEl = document.createElement('a');
+//     repoEl.classList = 'list-item flex-row justify-space-between align-center';
+//     repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
+
+//     var titleEl = document.createElement('span');
+//     titleEl.textContent = repoName;
+
+//     repoEl.appendChild(titleEl);
+
+//     var statusEl = document.createElement('span');
+//     statusEl.classList = 'flex-row align-center';
+
+//     if (repos[i].open_issues_count > 0) {
+//       statusEl.innerHTML =
+//         "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+//     } else {
+//       statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+//     }
+
+//     repoEl.appendChild(statusEl);
+
+//     repoContainerEl.appendChild(repoEl);
+//   }
+// };
+
+// userFormEl.addEventListener('submit', formSubmitHandler);
+// languageButtonsEl.addEventListener('click', buttonClickHandler);
